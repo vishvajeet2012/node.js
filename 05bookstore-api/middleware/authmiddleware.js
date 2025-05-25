1343b716fef661e35171fadd8c4380e04286ba0c
vishvajeet shukla <djritikshukla@gmail.com>
2025-05-25 13:55:14 +0530

    addded auth

--- 05bookstore-api/middleware/authmiddleware.js
+++ 05bookstore-api/middleware/authmiddleware.js
@@ -1,12 +1,29 @@
+const jwt =require('jsonwebtoken')
 const authMiddleware= async(req,res,next)=>{    
     console.log('this is auth middleware')
     const authHeader= req.headers['authorization']
-    if(!authHeader){
-        return res.status(401).json({
-            success:false,
-            message:"unauthorized user"
-        })
-    }
+        const token  = authHeader && authHeader.split('   ')[1]
+        if(!token){
+            return res.status(401).json({
+                success:false,
+                message:"unauthorized"
+            })
+        }
+try{
+const extractTokenInfo =jwt.verify(token,process.env.JWT_SECRET||"defaultsecret")
+req.user=extractTokenInfo
+console.log(req.user)
+req.userInfo = extractTokenInfo
+
+}
+catch(e){
+    console.log(e)
+    return res.status(401).json({
+        success:false,
+        message:"unauthorized"
+    })
+}
+
 
     next()
 
